7958b159d61b3c668ea68b2f3be24c81b65938ff
nasjones <nasjonez1@gmail.com>
2021-02-18 21:53:13 -0500

    endless pipe spawn added

--- src/index.js
+++ src/index.js
@@ -22,10 +22,9 @@ const config = {
 };
 
 let bird = null;
-let pipeX = 400;
 let pipes = null;
 
-const PIPE_VERT_DIST = [120, 250];
+const PIPE_VERT_DIST = [100, 200];
 const PIPE_HORI_DIST = [300, 400];
 const FLAPVELOCITY = 250;
 const BIRDORIGIN = { x: config.width * 0.1, y: config.height / 2 };
@@ -49,6 +48,10 @@ function create() {
 
 	this.input.on("pointerdown", flap);
 	this.input.keyboard.on("keydown_SPACE", flap);
+	this.input.keyboard.on("keydown-P", () => {
+		// console.log("working");
+		this.scene.pause();
+	});
 
 	pipes = this.physics.add.group();
 
@@ -56,22 +59,23 @@ function create() {
 		const hiPipe = pipes.create(0, 0, "pipe").setOrigin(0, 1);
 		const lowPipe = pipes.create(0, 0, "pipe").setOrigin(0, 0);
 
-		this.physics.add.collider(bird, hiPipe, restartBird);
-		this.physics.add.collider(bird, lowPipe, restartBird);
+		this.physics.add.collider(bird, hiPipe, resetBird);
+		this.physics.add.collider(bird, lowPipe, resetBird);
 
 		spawnPipe(hiPipe, lowPipe);
 	}
-	// console.log(pipes);
+
 	pipes.setVelocityX(-200);
 }
 
 function update() {
 	if (bird.body.blocked.none == false) {
-		restartBird();
+		resetBird();
 	}
+	recyclePipes();
 }
 
-function restartBird() {
+function resetBird() {
 	alert("you lost");
 	bird.body.y = BIRDORIGIN.y;
 	bird.body.velocity.y = 0;
@@ -82,21 +86,40 @@ function flap() {
 }
 
 function spawnPipe(hiPipe, lowPipe) {
-	let pipeSpace = Phaser.Math.Between(...PIPE_VERT_DIST);
-	let pipeY = Phaser.Math.Between(25, config.height - pipeSpace);
+	const rightMostX = getRightMostPipe();
+	const pipeSpace = Phaser.Math.Between(...PIPE_VERT_DIST);
+	const pipeY = Phaser.Math.Between(25, config.height - pipeSpace - 25);
+	const pipeX = Phaser.Math.Between(...PIPE_HORI_DIST);
 
-	hiPipe.x = pipeX;
+	hiPipe.x = rightMostX + pipeX;
 	hiPipe.y = pipeY;
 
-	lowPipe.x = pipeX;
+	lowPipe.x = hiPipe.x;
 	lowPipe.y = hiPipe.y + pipeSpace;
 
-	pipeX += Phaser.Math.Between(...PIPE_HORI_DIST);
-	// this.physics.add.collider(bird, pipeTop, restartBird);
-	// this.physics.add.collider(bird, pipeBottom, restartBird);
+	hiPipe.body.immovable = true;
+	lowPipe.body.immovable = true;
 }
 
-function getRightMostPipe() {}
+function recyclePipes() {
+	const tempPipes = [];
+	pipes.getChildren().forEach((pipe) => {
+		if (pipe.getBounds().right <= 0) {
+			tempPipes.push(pipe);
+			if (tempPipes.length == 2) {
+				spawnPipe(...tempPipes);
+			}
+		}
+	});
+}
+
+function getRightMostPipe() {
+	let rightMostX = 0;
+	pipes.getChildren().forEach((pipe) => {
+		rightMostX = Math.max(pipe.x, rightMostX);
+	});
+	return rightMostX;
+}
 
 new Phaser.Game(config);
 
